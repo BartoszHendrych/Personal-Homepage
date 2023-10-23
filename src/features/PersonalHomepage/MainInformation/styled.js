e1dde981134801338c95b6ea4da63fe260fa847a
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-10-23 23:13:29 +0200

    created styled file to index.js PersonalHomepage

---
+++ src/features/PersonalHomepage/MainInformation/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 64px;
+    align-items: center;
+`;
+
+export const Avatar = styled.img`
+    width: 22vw;
+    height: 380px;
+    border-radius: 50%;
+`;
+
+export const ThisIs = styled.div`
+    font-size: 12px;
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: initial;
+`;
+
+export const Name = styled.h1`
+    font-size: 38px;
+    font-weight: 900;
+    margin: 12px 0 0 0;
+`;
+
+export const Summary = styled.p`
+    max-width: 650px;
+    font-size: 20px;
+    line-height: 1.4;
+    margin: 36px 0 0 0;
+    color: #6E7E91;
+`;
