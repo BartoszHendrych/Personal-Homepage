63557fb1571ba875222599c9dc189f58f2405fc0
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-10-24 20:50:29 +0200

    corrected file styled.js ButtonLink

--- src/features/PersonalHomepage/ButtonLink/index.js
+++ src/features/PersonalHomepage/ButtonLink/index.js
@@ -1,30 +1,29 @@
-import styled, { css } from "styled-components";
-import { ReactComponent as MessageIcon} from "./Message.svg";
+import styled from "styled-components";
 
-export const ButtonLink = styled.button`
+export const ButtonLink = styled.a`
     display: inline-flex;
-    padding: 12px 16px;
     margin-top: 32px;
+    padding: 12px 16px;
+    gap: 16px;
     justify-content: center;
     align-items: center;
-    gap: 16px;
     border-radius: 4px;
     border: 1px solid;
     background: #0366D6;
     color: #ffffff;
-    font-weight: 600;
-    font-size: 20.058px;
-    box-shadow: 0px 2px 0px 0px;
+    text-align: center;
+    font-size: 21px;
+    font-style: normal;
     cursor: pointer;
+    text-decoration: none;
 
     &:hover {
+        box-shadow: 0px 0px 0px 2px;
         background: #2188FF;
     }
-`;
 
-export const Icon = styled(MessageIcon)`
-
-    ${({ moveToRight}) => moveToRight && css`
-        transform: translateX(16px);
-    `}
+    @media(max-width: 767px) {
+        font-size: 18px;
+        gap: 12px;
+    }
 `;
