3c60d7613171f6973577b17d90c5f048985a856f
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-10-24 13:07:12 +0200

    main file created in the personalHomepage folder

---
+++ src/features/PersonalHomepage/index.js
@@ -0,0 +1,10 @@
+import { Wrapper } from "./styled";
+import { ThemeSwitch } from "./ThemeSwitch";
+import { MainInformation } from "./MainInformation";
+
+export const PersonalHomepage = () => (
+    <Wrapper>
+        <ThemeSwitch />
+        <MainInformation />
+    </Wrapper>
+)
