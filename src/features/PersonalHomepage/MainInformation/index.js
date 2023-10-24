6aad6c00185ebb846cb855b431ef52e46f317ab8
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-10-24 13:09:52 +0200

    Adding ButtonLink to MainInformation

--- src/features/PersonalHomepage/MainInformation/index.js
+++ src/features/PersonalHomepage/MainInformation/index.js
@@ -1,5 +1,6 @@
 import { Avatar, Summary, ThisIs, Wrapper, Name } from "./styled";
 import BartoszHendrychProfile from "./zdjCV.jpg";
+import { ButtonLink, Icon } from "../ButtonLink";
 
 export const MainInformation = () => (
     <Wrapper>
@@ -11,6 +12,11 @@ export const MainInformation = () => (
                I'm a passionate front-end developer who finds their calling in creating interactive and aesthetically
                pleasing websites and applications. 
             </Summary>
+
+            <ButtonLink>
+                <Icon />
+                Hire me
+            </ButtonLink>
         </div>
     </Wrapper>
 )
