e54f692363229bc895df76d3965ef26a2e686ca4
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-10-27 15:21:01 +0200

    deleted icon FB

--- src/features/PersonalHomepage/Footer/SocialIcons/socials.js
+++ src/features/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -1,6 +1,5 @@
 import { styleIcon } from "./styled";
 import { ReactComponent as GitHubIcon  } from "./icons/GitHub.svg";
-import { ReactComponent as FBIcon  } from "./icons/FB.svg"
 import { ReactComponent as LinkedInIcon  } from "./icons/LinkedIN.svg";
 
 export const socials = [
@@ -8,11 +7,6 @@ export const socials = [
         name: "GitHub",
         url: "https://github.com/BartoszHendrych",
         Icon: styleIcon(GitHubIcon),
-    },
-    {
-        name: "Facebook",
-        url: "https://www.facebook.com/bartek.hendrych/",
-        Icon: styleIcon(FBIcon),
     },
     {
         name: "LinkedIn",
