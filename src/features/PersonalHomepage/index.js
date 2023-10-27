2aecce0cdc041cdd430f1f5ece92b27249c0bda2
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-10-27 12:25:48 +0200

    Added Skills to personalHomepage/index.js

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -3,11 +3,13 @@ import { ThemeSwitch } from "../../common/ThemeSwitch";
 import { MainInformation } from "./MainInformation";
 import { Footer } from "./Footer";
 import { Portfolio } from "./Portfolio";
+import { Skills } from "./Skills";
 
 export const PersonalHomepage = () => (
     <Wrapper>
         <ThemeSwitch />
         <MainInformation />
+        <Skills />
         <Portfolio />
         <Footer />
     </Wrapper>
