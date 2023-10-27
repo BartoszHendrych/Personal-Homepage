d3da75dfe093fb4f32a21f8940f19f5a477beae9
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-10-27 12:37:32 +0200

    modified file App/index.js

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,5 +1,20 @@
+import { useSelector } from "react-redux"
+import { selectIsDarkTheme } from "../../common/themeSlice"
+import { ThemeProvider } from "styled-components";
+import { themeDark, themeLight } from "./theme";
 import { PersonalHomepage } from "../../features/PersonalHomepage";
+import { GlobalStyle } from "./GlobalStyle";
 
-export const App = () => (
-    <PersonalHomepage />
-)
+
+const App = () => {
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+
+    return (
+        <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+            <GlobalStyle />
+            <PersonalHomepage />
+        </ThemeProvider>
+    );
+}
+
+export default App;
