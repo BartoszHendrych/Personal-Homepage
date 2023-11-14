18c8bf50f14b1a4ecdc4d77a1555e929d950d92b
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-11-14 17:20:40 +0100

    modification of the App/store.js file

--- src/core/store.js
+++ src/core/store.js
@@ -1,19 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { PersonalHomepage } from '../features/PersonalHomepage';
 import themeReducer from '../common/themeSlice';
-import createSagaMiddleware from 'redux-saga';
-import saga from './saga';
-
-const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
-        personalHomepage: PersonalHomepage,
         theme: themeReducer,
     },
-    middleware: [sagaMiddleware],
 })
-
-sagaMiddleware.run(saga);
 
 export default store;
