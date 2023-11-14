90c319c2457e6edffffab50efe82c3ee15eefa08
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-11-14 21:42:05 +0100

    added githubProfile path

--- src/features/PersonalHomepage/data.js
+++ src/features/PersonalHomepage/data.js
@@ -1,3 +1,5 @@
 export const email = "bartek.hendrych@gmail.com";
 
 export const githubApi = "https://api.github.com/users/BartoszHendrych/repos";
+
+export const githubProfile = "https://github.com/BartoszHendrych";
