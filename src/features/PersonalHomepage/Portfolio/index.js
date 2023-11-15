62bb6bd1bff9f4afd0cb351e6c139b867f0a7e23
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-11-15 13:57:51 +0100

    stylized and adding interaction to Porfolio

--- src/features/PersonalHomepage/Portfolio/index.js
+++ src/features/PersonalHomepage/Portfolio/index.js
@@ -1,11 +1,38 @@
-import { Section, Header, StyledGitHubIcon, MyRecentProjects, Title } from "./styled";
+import { StyledIconGitHub, Wrapper, Section, Header, Description } from "./styled";
+import { useState, useEffect } from "react";
+import { Content } from "./Content";
+import { getRepositories } from "./getRepositories";
 
-export const Portfolio = () => (
-    <Section>
-        <Header>
-            <StyledGitHubIcon />
-            <Title>Portfolio</Title>
-            <MyRecentProjects>My recent projects</MyRecentProjects>
-        </Header>
-    </Section>
-);
+export const Portfolio = () => {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    setProjects("loading");
+
+    const timeoutId = setTimeout(() => {
+      getRepositories()
+        .then((projects) => {
+          setProjects(projects);
+        })
+        .catch(() => {
+          setProjects("error");
+        });
+    }, 1000); //to show loader;
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <Wrapper>
+      <Section>
+        <StyledIconGitHub />
+        <Header>Portfolio</Header>
+        <Description>My recent projects</Description>
+      </Section>
+      
+      <Content projects={projects} />
+    </Wrapper>
+  );
+};
--- src/features/PersonalHomepage/Portfolio/styled.js
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -1,46 +1,41 @@
-import styled from "styled-components";
-import { ReactComponent as GitHubIcon} from "./GitHubPortfolio.svg";
+import { styled } from "styled-components";
+import { ReactComponent as IconGitHub } from "./GitHubPortfolio.svg";
+
+export const Wrapper = styled.section`
+  margin-top: 72px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+      margin-top: 48px;
+    }
+`;
 
 export const Section = styled.section`
-    margin-top: 72px;
-
-    @media(max-width: 767px) {
-        margin-top: 48px;
-    }
-`;
- 
-export const StyledGitHubIcon = styled(GitHubIcon)`
-    color: ${({ theme }) => theme.colors.primary};
-    margin-bottom: 8px;
+   text-align: center;
+   margin: 0;
 `;
 
-export const Header = styled.header`
-    text-align: center;
+export const StyledIconGitHub = styled(IconGitHub)`
+  color: ${({ theme }) => theme.colors.primary};
+  margin-bottom: 12px;
 `;
 
-export const MyRecentProjects = styled.h3`
-    font-size: 20px;
-    font-weight: 400;
-    color: ${({ theme }) => theme.colors.textPrimary};
-    text-align: center;
-    margin-top: 8px;
-    margin-bottom: 24px;
+export const Header = styled.h2`
+  margin: 0;
+  font-size: 30px;
+  font-weight: 900;
+  color: ${({ theme }) => theme.colors.textPrimary};
 
-    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-       font-size: 17px;
-       margin-top: 16px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 18px;
+  }
 `;
 
-export const Title = styled.p`
-    margin-top: 12px;
-    margin: 0%;
-    font-size: 30px;
-    font-style: normal;
-    font-weight: bold;
-    color: ${({ theme }) => theme.colors.textPrimary};
+export const Description = styled.p`
+  margin-top: 8px;
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.textPrimary};
 
-    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-        font-size: 18px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 17px;
+  }
 `;
