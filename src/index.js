15a187dc9c8a0067ac400bcccd5a374dfbdf9828
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-11-20 17:38:25 +0100

    unnecessary link removed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './core/App';
-import { GlobalStyle } from './core/App/GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from "./core/store"
